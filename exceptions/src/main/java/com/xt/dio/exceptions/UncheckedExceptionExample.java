0c7a9a62d3952669e5938acf5395b9d66c3e8abb
Lucas Oliveira <lucaoxt@gmail.com>
2024-04-18 10:26:09 -0300

    UncheckedExceptionExample

---
+++ exceptions/src/main/java/com/xt/dio/exceptions/UncheckedExceptionExample.java
@@ -0,0 +1,42 @@
+package com.xt.dio.exceptions;
+
+import javax.swing.*;
+
+public class UncheckedExceptionExample {
+
+    public static void main(String[] args) {
+        boolean continueLooping = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try{
+                int result = divide(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + result);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane
+                    .showMessageDialog(
+                        null,
+                        "Entrada inválida, informe um número inteiro! " + e.getMessage()
+                    );
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane
+                    .showMessageDialog(
+                        null,
+                        "Impossível dividir um número por 0."
+                    );
+            }
+            finally {
+                System.out.println("Chegou no finally!");
+            }
+        } while(continueLooping);
+        System.out.println("O código continua...");
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
